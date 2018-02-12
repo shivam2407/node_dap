2a7ac411e48ee3e682963abff0101fc52cb0b6b7
Shivam <rawat.s@husky.neu.edu>
2018-02-12 02:43:35 -0500

    Filter http request

--- Node-http/index.js
+++ Node-http/index.js
@@ -1,14 +1,44 @@
 const http = require('http');
-
+const path = require('path');
+const fs = require('fs');
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,res) => {
     console.log(req.headers);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Hey it works</h1></body></html>')
+    if(req.method == "GET"){
+        var fileUrl;
+        if(req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+                if(!exists){
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text/html');
+                    res.end('<html><body><h1> Wrong url bro!</h1></body></html>');
+                }
+                else{
+                    res.statusCode=200
+                    res.setHeader('Content-Type','text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                }
+            });
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type','text/html');
+            res.end('<html><body><h1> Not a html file bro!</h1></body></html>');
+        }
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type','text/html');
+        res.end('<html><body><h1> Only get method!</h1></body></html>');
+    }
 });
 
 server.listen(port, hostname, () => {
